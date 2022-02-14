630226381339a4f54836594172b02607eb7a308f
Monique Rio <mrio@umich.edu>
2022-02-14 16:14:23 -0500

    added pushgateway ping for these scripts

--- pull_last_month_zephir_full.rb
+++ pull_last_month_zephir_full.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 require_relative 'lib/pull_from_zephir'
 require 'dotenv'
+start_time_stamp=`date '+%s'`
 Dotenv.load('.env')
 
 PullFullFromLastMonthFromZephir.new.run
+
+if ENV.fetch('SCRIPT_ENV') == 'production'
+  `/usr/local/bin/pushgateway -j search_hathi_trust_monthly_pull -b #{start_time_stamp}`
+end
--- pull_latest_zephir_update.rb
+++ pull_latest_zephir_update.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require_relative 'lib/pull_from_zephir'
 require 'dotenv'
+start_time_stamp=`date '+%s'`
 Dotenv.load('.env')
 
 PullDailyFromZephir.new.run
@@ -9,3 +10,8 @@ begin
 rescue SocketError => e
   Rails.logger.error("Failed to contact Pushmon; #{e.to_s}")
 end
+
+if ENV.fetch('SCRIPT_ENV') == 'production'
+  `/usr/local/bin/pushgateway -j search_hathi_trust_daily_pull -b #{start_time_stamp}`
+end
+
